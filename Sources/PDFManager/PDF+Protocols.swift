d226c7c78dbf6cd14bae51f1dcc08a20590188b7
Mark Battistella <markbattistella@users.noreply.github.com>
2025-11-01 17:41:52 +1100

    2025-11-01

--- Sources/PDFManager/PDF+Protocols.swift
+++ Sources/PDFManager/PDF+Protocols.swift
@@ -39,8 +39,8 @@ extension PDFHeader {
 /// A view type that defines the main content section for a page in a generated PDF. Conforming
 /// types render the core page body content.
 public protocol PDFContent: View {
-    associatedtype Data: Identifiable
-    var items: [Data] { get }
+    associatedtype T: Identifiable
+    init(items: [T])
 }
 
 // MARK: - Footer
